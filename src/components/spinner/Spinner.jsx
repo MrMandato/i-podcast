a637b35dad64081e7248c0281b1a12b701330b4b
williams Blanco <williamsblanco216@gmail.com>
2023-09-10 14:22:12 -0400

    🎨 feat: create spinner component

---
+++ src/components/spinner/Spinner.jsx
@@ -0,0 +1,12 @@
+import './spinner.css'
+
+export const Spinner = () => {
+  return (
+    <div className="wrapper-spinner" >
+      <div className="spinner">
+
+      </div>
+      <h3>loading</h3>
+    </div >
+  ) 
+}  
