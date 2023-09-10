45bdfcf5fe585697ead05a4033d83d2963ffcb4b
williams Blanco <williamsblanco216@gmail.com>
2023-09-10 16:07:49 -0400

    🔇 chore: remove console.logs

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,9 @@ import { Spinner } from './components/spinner'
 
 import './app.css'
 import { useSpinner } from "./hooks/useSpinner"
-import { useEffect } from "react"
 
 function App() {
   const [isLoading] = useSpinner()
-  useEffect(() => { console.log('cambio app', isLoading) }, [isLoading])
-  console.log('app is loading', isLoading)
   return (
     <>
       {isLoading && < Spinner />}
