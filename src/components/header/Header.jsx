7316c73dbca1afebba94125a9b46fa9624756376
Orlando Lopez <1707orlandoalex@gmail.com>
2024-06-18 20:29:53 -0400

    [WO-23051] - fixed div to header in Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,10 +3,10 @@ import './header.css'
 export const Header = () => {
   /* TODO: cambiar la etiqueta div por header */
   return (
-    <div className="i-podcast__header">
+    <header className="i-podcast__header">
       <h1>
         i-Podcast
       </h1>
-    </div>
+    </header>
   )
 }
